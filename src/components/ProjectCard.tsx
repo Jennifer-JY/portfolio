0b7ceea0974a45cd10b6fb3d98cd531f988a4b50
Jennifer-JY <yujiayuejy@gmail.com>
2025-08-29 20:24:14 +1000

    debugging deploy

--- src/components/ProjectCard.tsx
+++ src/components/ProjectCard.tsx
@@ -54,7 +54,11 @@ export function ProjectCard({
       </section>
 
       {imgRef && (
-        <img src={imgRef} alt="demo" className="w-1/3 h-auto rounded-md" />
+        <img
+          src={"/portfolio/" + imgRef}
+          alt="demo"
+          className="w-1/3 h-auto rounded-md"
+        />
       )}
     </article>
   );
